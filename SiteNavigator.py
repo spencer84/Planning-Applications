2b4f07d35add6a363bc93d582a4e326ee1b05e47
Samuel Spencer <36893802+spencer84@users.noreply.github.com>
2022-09-23 09:53:08 +0100

    Add files via upload

---
+++ SiteNavigator.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+class SiteNavigator:
+    """
+    A class to navigate and parse the contents of a given website.
+    """
+    def __init__(self):
+        self.site = None
+        self.driver = None
+        self.current_page = None
+        self.search_results = []
+
+    def open_page(self):
+        """
+        Attempt to return to the given site using the webdriver object from Selenium
+        :return: driver object opened to the site
+        """
+        driver = webdriver.Chrome("chromedriver") # Need to store the chromedriver file to local directory
+        driver.get(self.site)
+        # There may be some commonalities across different types of planning pages;
+        # Perhaps in the future this function could also identify whether a given site fits into any previously
+        # observed patterns for a planning application website.
+        self.driver = driver
+
---
+++ getplanningapps.py
@@ -0,0 +1,80 @@
+import time
+from SiteNavigator import SiteNavigator
+import selenium.common.exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+# Open a Chrome browser and navigate to the council planning page
+# site = 'https://www.cheshirewestandchester.gov.uk/residents/planning-and-building-control/' \
+#        'see-or-comment-on-planning-applications.aspx'
+
+site = 'https://pa.cheshirewestandchester.gov.uk/online-applications/search.do?action=advanced'
+
+# Eventually have these reflect the most recent record to the current date.
+start_date = '01/07/2022'
+end_date = '08/07/2022'
+
+
+class ApplicationNavigator(SiteNavigator):
+    """
+    Inherited from the SiteNavigator class, this class is designed specifically for parsing Planning Applications stored
+    on a council's website.
+    """
+    def agree(self):
+        """
+        Agree to any terms/agreements that may be at the page
+        :return: None
+        """
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id="ccc-reject-settings"]'))).click()
+
+    def search(self):
+        """
+        Search by a given date range
+        :return:
+        """
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.XPATH, "//*[@id=\"applicationDecisionStart\"]"))).send_keys(start_date)
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.XPATH, "//*[@id=\"applicationDecisionEnd\"]"))).send_keys(
+            end_date)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/div/div/div[3]/div[3]/div[2]/form/div[4]/input[2]'))).click()
+
+        # Store the first page of results
+        self.current_page = self.driver.current_url
+        self.driver
+        self.search_results = self.driver.find_elements_by_class_name("searchresult")
+        print(self.search_results)
+
+
+    def add_results(self):
+        """For every search result on a page, visit each link, then parse content to a database"""
+        for i in self.search_results:
+            link = i.find_element_by_tag_name('a')
+            link.click()
+            # Get data here
+            # Return to search result page
+            self.driver.get(self.current_page)
+
+    # def next_page(self):
+    #
+    #
+    #
+    #
+    # def find_apps(self):
+    #     """
+    #
+    #     :return:
+    #     """
+
+
+if __name__ == "__main__":
+    nav = ApplicationNavigator()
+    nav.site = site
+    nav.open_page()
+    nav.search()
+    nav.add_results()
