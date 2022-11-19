000431464c0fe6a2617aabd26cc462b00bb9a052
Sam Spencer <sam95@vt.edu>
2022-11-19 21:51:22 +0000

    Setting up the foundation for the class to connect to a MySql database

--- dbconnect.py
+++ dbconnect.py
@@ -1,1 +1,8 @@
+from creds import username, password
 import mysql.connect
+
+class dbconnect:
+    def __init__(self):
+        self.connection = None
+    def connect(username = username, password = password, host = "127.0.0.0"):
+        """Connect to a database using stored credentials"""
