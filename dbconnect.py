e2c57c369318810fa02cd764b90141e315562236
Sam Spencer <sam95@vt.edu>
2022-11-19 22:39:05 +0000

    Added try/except section to improve handling the connection. Also added messages to identify if connected.

--- dbconnect.py
+++ dbconnect.py
@@ -1,8 +1,23 @@
 from creds import username, password
-import mysql.connect
+import mysql.connector
+from mysql.connector import Error
 
 class dbconnect:
     def __init__(self):
         self.connection = None
-    def connect(username = username, password = password, host = "127.0.0.0"):
+        # Establish the database connection
+        self.connect()
+        self.cursor = self.connection.cursor()
+
+    def connect(self):
         """Connect to a database using stored credentials"""
+        try:
+            self.connection = mysql.connector.connect(username = username, password = password, host = "127.0.0.0", database = "planning")
+            if self.connection.is_connected():
+                print("Successfully connected to MySQL database.")
+        except Error as e:
+            print("Error connecting to MySQL database: "+ e.msg)
+    def importApplication(self, application):
+        """Import the details of a planning application into the MySQL database"""
+        self.cursor.execute()
+    
