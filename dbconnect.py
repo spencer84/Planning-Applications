e94f2a5455724e662d40dac4f3984f5ac617be9f
Sam Spencer <sam95@vt.edu>
2022-11-21 22:58:08 +0000

    Switched to Mariadb due to frustrating issues in MySQL set-up. Just need something to work for now and this should be functionally the same.

--- dbconnect.py
+++ dbconnect.py
@@ -1,23 +1,27 @@
 from creds import username, password
-import mysql.connector
-from mysql.connector import Error
+import mariadb
 
 class dbconnect:
     def __init__(self):
-        self.connection = None
+        self.connection = self.connect()
         # Establish the database connection
-        self.connect()
         self.cursor = self.connection.cursor()
 
     def connect(self):
         """Connect to a database using stored credentials"""
+        print(f"Attempting to connect using username {username} and password {password}")
         try:
-            self.connection = mysql.connector.connect(username = username, password = password, host = "127.0.0.0", database = "planning")
-            if self.connection.is_connected():
-                print("Successfully connected to MySQL database.")
-        except Error as e:
-            print("Error connecting to MySQL database: "+ e.msg)
+            self.connection = mariadb.connect(user = username, host ='127.0.0.1', port = 3306,  password = password, database = "planning")
+            print("Successfully connected to MySQL database.")
+        except Exception as e:
+            print("Error connecting to MySQL database: "+ e.msg) 
+
     def importApplication(self, application):
         """Import the details of a planning application into the MySQL database"""
         self.cursor.execute()
+
+if __name__ == "__main__":
+    print("testing...")
+    test = dbconnect()
+    
     
