bf11f133c605b0229d0ebcd7bacdda256aeba86f
Sam Spencer <sam95@vt.edu>
2022-11-23 19:52:37 +0000

    Added full fields to be retrieved

--- PlanningApplication.py
+++ PlanningApplication.py
@@ -2,6 +2,12 @@
 class PlanningApplication:
     def __init__(self):
         self.reference = None
-        self.date = None
+        self.alt_reference = None
+        self.date_received = None
+        self.address = None
+        self.proposal = None
+        self.status = None
+        self.appeal = None
+        self.appeal_status = None
     #     self.db =
     # def sendToDatabase(self):
