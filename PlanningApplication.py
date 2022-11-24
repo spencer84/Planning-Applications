fd296f9e2701ff6e2fd321f69c800995d8cf304c
Sam Spencer <sam95@vt.edu>
2022-11-24 18:31:34 +0000

    Added function to print all atributes (for testing)

--- PlanningApplication.py
+++ PlanningApplication.py
@@ -9,5 +9,18 @@ class PlanningApplication:
         self.status = None
         self.appeal = None
         self.appeal_status = None
+    def printAttributes(self):
+        print(
+            """Reference Number: {self.reference} 
+            Alternative Ref Number: {self.alt_reference}
+            Date Received: {self.date_received}
+            Address: {self.address}
+            Proposal: {self.proposal}
+            Status: {self.status}
+            Appeal: {self.appeal}
+            Appeal Status: {self.appeal_status}
+            """
+
+        )
     #     self.db =
     # def sendToDatabase(self):
