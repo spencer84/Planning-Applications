433e7f5e28cc657b3b5c68fe23c73fca68e01767
Sam Spencer <sam95@vt.edu>
2022-12-12 22:54:55 +0000

    First basic method to return last week of decisions

--- webapp.py
+++ webapp.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+from dbconnect import dbconnect
+import datetime
 
 app = FastAPI()
 
-@app.get
+# Connect to database
+conn = dbconnect()
+
+@app.get("/decided-last-week")
+def weekly_list():
+    """Return a list of weekly planning applications"""
+    cur = conn.cursor
+    last_week = datetime.datetime.now() - datetime.timedelta(days=7)
+    cur.execute(f"""SELECT * FROM applications WHERE DecisionDate >= {last_week.strftime("%Y/%m/%d")}""")
+    results = cur.fetchall()
+    return results
+
+
+
