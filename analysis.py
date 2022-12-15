4130300198906dae9131204cc65392cf3dbc63aa
Sam Spencer <sam95@vt.edu>
2022-12-15 22:18:06 +0000

    Putting together ideas for further analysis

---
+++ analysis.py
@@ -0,0 +1,16 @@
+
+# Perform additional analysis on the Planning applications
+
+# 1. Geo-locate 
+#   - functionality to identify exact location of application 
+#   - Nearby postcodes
+
+# Create a table for more address details (i.e postcode, lat/long, etc)
+# 
+
+
+# 2. Build NLP Model to interpret the type of application
+
+# 3. Build database of appeals records
+
+# 4. Extrapolate number of housing units proposed
