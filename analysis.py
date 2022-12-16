68ba15cb8de1f25c2d2e38e3056a9a3df386c7c7
Sam Spencer <sam95@vt.edu>
2022-12-16 21:29:29 +0000

    Just more notes

--- analysis.py
+++ analysis.py
@@ -1,13 +1,15 @@
 
+from dbconnect import dbconnect
 # Perform additional analysis on the Planning applications
 
 # 1. Geo-locate 
+#   - parse address field to extract a postcode if possible
+db = dbconnect()
+db.connection.cursor()
 #   - functionality to identify exact location of application 
 #   - Nearby postcodes
 
 # Create a table for more address details (i.e postcode, lat/long, etc)
-# 
-
 
 # 2. Build NLP Model to interpret the type of application
 
