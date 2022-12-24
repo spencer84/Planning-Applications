7af40c5259be8952a7eb5ae633785531d69d2568
Sam Spencer <sam95@vt.edu>
2022-12-24 21:31:25 +0000

    Improving nearby postcodes request

--- webapp.py
+++ webapp.py
@@ -16,7 +16,7 @@ async def read_index():
 
 @app.get("/decided-last-week")
 def weekly_list():
-    """Return a list of weekly planning applications"""
+    """Return a list of weekly planning applications -- decided in the last week"""
     cur = conn.cursor
     last_week = datetime.datetime.now() - datetime.timedelta(days=7)
     cur.execute(f"""SELECT * FROM applications WHERE DecisionDate >= {last_week.strftime("%Y/%m/%d")}""")
@@ -28,8 +28,13 @@ def near_me(postcode, range=7):
     """Return a list of all postcodes near the inputed postcode"""
     results = requests.get(f"api.postcodes.io/{postcode}/nearest")
     nearby = [x.get('postcode') for x in results]
+    codes = ""
+    for postcode in nearby:
+        codes += postcode
+        if postcode != nearby[-1]:
+            codes += " "
     cur = conn.cursor
-    cur.execute(f"""SELECT * FROM applications WHERE """)
+    cur.execute(f"""SELECT * FROM applications WHERE postcode in ({codes})""")
 
 
 
