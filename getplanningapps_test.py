d1eff4acbc79ad91617f092684cdcbb9954fbcf4
Sam Spencer <sam95@vt.edu>
2023-01-05 23:15:04 +0000

    Outline of two simple tests and notes

--- getplanningapps_test.py
+++ getplanningapps_test.py
@@ -1,6 +1,16 @@
 import unittest
+import dbconnect
+from PlanningApplication import PlanningApplication
 
 # Test out the ability to accurately update planning application data
-#
 
 class TestPlanningApplication(unittest.TestCase):
+    def test_update():
+        """Check to confirm that old data in the database is replaced by more recent data"""
+        new_app = PlanningApplication()
+    def test_update_submitted():
+        """A 'submitted' record shall be updated by elements of a 'decided' record"""
+
+
+if __name__ == "__main__":
+    unittest.main()
