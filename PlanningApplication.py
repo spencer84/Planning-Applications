9affeecbf482576966fe1e8170e82e7aff5bae4c
Sam Spencer <sam95@vt.edu>
2023-01-17 18:58:47 +0000

    Add link to db submit and print statement

--- PlanningApplication.py
+++ PlanningApplication.py
@@ -47,6 +47,7 @@ class PlanningApplication:
             Decision Date: {self.decision_date}
             Appeal: {self.appeal}
             Appeal Status: {self.appeal_status}
+            Link: {self.link}
             """
         )
             
@@ -55,15 +56,16 @@ class PlanningApplication:
         """Either update or insert new record"""
         cur = connection.cursor()
         cur.execute(f"""INSERT INTO applications (ReferenceNumber, LocalPlanningAuthority, AlternativeReferenceNumber, DateReceived,
-        Address, Proposal, Status, Decision, DecisionDate, Appeal, AppealStatus, DateDataRetrieved, Postcode) values ('{self.reference}', '{self.local_planning_authority}','{self.alt_reference}',
+        Address, Proposal, Status, Decision, DecisionDate, Appeal, AppealStatus, DateDataRetrieved, Postcode, Link) values ('{self.reference}', '{self.local_planning_authority}','{self.alt_reference}',
         '{self.date_received}','{self.address}','{self.proposal}','{self.status}','{self.decision}','{self.decision_date}','{self.appeal}',
-        '{self.appeal_status}', '{self.date_collected}', '{self.postcode}')
+        '{self.appeal_status}', '{self.date_collected}', '{self.postcode}', '{self.link}')
         ON DUPLICATE KEY UPDATE DateDataRetrieved ='{self.date_collected}',
         Decision = '{self.decision}',
         DecisionDate = '{self.decision_date}', 
         Status = '{self.status}', 
         Appeal = '{self.appeal}', 
-        AppealStatus = '{self.appeal_status}'
+        AppealStatus = '{self.appeal_status}', 
+        Link = '{self.link}'
         """)
         print("Successfully inserted row")
         connection.commit()
