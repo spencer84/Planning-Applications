f12555bb560d534fd196fc72c091601f0c074e44
Sam Spencer <sam95@vt.edu>
2023-03-02 07:27:24 +0000

    Using the new json query function

--- webapp.py
+++ webapp.py
@@ -21,19 +21,15 @@ async def read_index():
 @app.get("/decided-last-week")
 def weekly_list():
     """Return a list of weekly planning applications -- decided in the last week"""
-    cur = conn.cursor
     last_week = datetime.datetime.now() - datetime.timedelta(days=7)
-    cur.execute(f"""SELECT * FROM applications WHERE DecisionDate >= {last_week.strftime("%Y/%m/%d")}""")
-    results = cur.fetchall()
+    results = conn.queryJSON(f"""SELECT * FROM applications WHERE DecisionDate >= {last_week.strftime("%Y/%m/%d")}""")
     return results
 
 @app.get("/received-last-week")
 def sub_weekly_list():
     """Return a list of weekly planning applications -- received in the last week"""
-    cur = conn.cursor
     last_week = datetime.datetime.now() - datetime.timedelta(days=7)
-    cur.execute(f"""SELECT * FROM applications WHERE DateReceived >= {last_week.strftime("%Y/%m/%d")}""")
-    results = cur.fetchall()
+    results = conn.queryJSON(f"""SELECT * FROM applications WHERE DateReceived >= {last_week.strftime("%Y/%m/%d")}""")
     return results
 
 @app.post("/near-me/")
@@ -48,9 +44,7 @@ async def near_me(postcode: str = Form()):
         if postcode != nearby[-1]:
             codes += "\', \'"
     codes += '\''
-    cur = conn.cursor
-    print(codes)
-    cur.execute(f"""SELECT * FROM applications WHERE postcode in ({codes})""")
+    conn.queryJSON(f"""SELECT * FROM applications WHERE postcode in ({codes})""")
     applications = cur.fetchall()
     if len(applications) == 0:
         return "There are no nearby planning applications"
